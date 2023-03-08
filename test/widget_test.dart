9024be0e635ebd8daa94c12f234bff84e06ab0ff
Hamza <hamza.afzalkhan9@gmail.com>
2023-03-08 20:40:26 +0500

    modified test removed const from myApp constructor

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,3 @@
-// This is a basic Flutter widget test.
 //
 // To perform an interaction with a widget in your test, use the WidgetTester
 // utility in the flutter_test package. For example, you can send tap and scroll
@@ -13,7 +12,7 @@ import 'package:appbar4/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
